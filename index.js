3c1b93ef76b7ac8ef7b71a69acd74b178c6afd28
guoaihua <308232015@qq.com>
2024-12-19 22:29:39 +0800

    feat: update message check interface

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
 const axios = require("axios");
+const request = require("request");
 const fs = require("fs");
 const path = require("path");
 const multiparty = require("multiparty");
+const bodyParser = require("body-parser");
+const FormData = require("form-data");
+
 const themesMap = {};
+let accessToken = "";
+
 // const sqlite3 = require('sqlite3').verbose();
 
 // // 创建数据库连接
@@ -21,6 +27,13 @@ const themesMap = {};
 
 // // 关闭数据库连接
 // db.close();
+
+app.use(bodyParser.urlencoded({ extended: false, limit: "2mb" }));
+app.use(
+  bodyParser.json({
+    limit: "2mb",
+  })
+);
 
 const commonPicLibs = () => {
   // const ddServerImagePath = "/Users/ziming/Desktop/dd-photo-pics";
@@ -48,10 +61,30 @@ const commonPicLibs = () => {
 };
 commonPicLibs();
 
+// 获取微信access_token
+const refreshToken = () => {
+  axios
+    .get("https://api.weixin.qq.com/cgi-bin/token", {
+      params: {
+        grant_type: "client_credential",
+        appid: "wx69abccfa61662325",
+        secret: "18e0cd0fcb42ccf28fc2faffc7d42006",
+      },
+    })
+    .then(({ data }) => {
+      console.log(data);
+      accessToken = data?.access_token;
+    });
+};
+refreshToken();
+
 setInterval(() => {
-  //  一个小时手动刷新一下， 防止图片库更新
+  //  2个小时手动刷新一下， 防止图片库更新
   commonPicLibs();
-}, 1000 * 60 * 60);
+  // access_token 的有效期目前为 2 个小时，需定时刷新，重复获取将导致上次获取的 access_token 失效；
+  // 中控服务器去刷新token，同时刷新容易导致相互覆盖，多个pod最后只有一个生效
+  refreshToken();
+}, 1000 * 60 * 60 * 2);
 
 app.use(express.static("images"));
 
@@ -71,13 +104,72 @@ app.get("/getPics", (req, res) => {
   });
 });
 
+app.get("/login", (req, res) => {
+  console.log(req.query);
+
+  axios
+    .get("https://api.weixin.qq.com/sns/jscode2session", {
+      params: {
+        grant_type: "authorization_code",
+        appid: "wx69abccfa61662325",
+        secret: "18e0cd0fcb42ccf28fc2faffc7d42006",
+        js_code: req.query.code,
+      },
+    })
+    .then(({ data }) => {
+      res.send(data);
+    });
+});
+
+app.post("/checkImg", (req, res) => {
+  const imgBuffer = Buffer.from(req.body.img, "base64");
+  request(
+    {
+      method: "post",
+      url: `https://api.weixin.qq.com/wxa/img_sec_check?access_token=${accessToken}`,
+      formData: {
+        media: {
+          options: {
+            filename: "topsecret.jpg",
+            contentType: "image/png",
+          },
+          value: imgBuffer,
+        },
+      },
+    },
+    (error, response, body) => {
+      console.log(error, body);
+      res.send(body);
+    }
+  );
+});
+
+app.get("/checkMsg", (req, res) => {
+  request(
+    {
+      method: "post",
+      url: `https://api.weixin.qq.com/wxa/msg_sec_check?access_token=${accessToken}`,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      json: true,
+      body: {
+        content: req.query.message,
+      },
+    },
+    (error, response, body) => {
+      res.send(body);
+    }
+  );
+});
+
 app.post("/addPic", (req, res) => {
   var form = new multiparty.Form({
     uploadDir: path.resolve(__dirname, "./images"),
   });
   const namesMap = {
-    dog: '狗'
-  }
+    dog: "狗",
+  };
   form.parse(req, function (err, fields, files) {
     console.log(files);
     if (Array.isArray(files?.pics)) {
@@ -92,10 +184,6 @@ app.post("/addPic", (req, res) => {
         console.log(url);
       });
     }
-  });
-
-  res.send({
-    message: "ok",
   });
 });
 
